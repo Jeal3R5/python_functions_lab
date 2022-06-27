6ea5c2411543cea54ba3d0eb721288d4c55ec272
Joan Renfroe <joan.renfroe@gmail.com>
2022-06-26 19:20:39 -0700

    Initial commit

---
+++ py_func_lab.py
@@ -0,0 +1,44 @@
+# ----- Exercise 1 -----
+#write func named sum_to that accepts a single integer, n, and returns the sum of the integers from 1 to n
+
+def sum_to(n):
+    return sum()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ----- Exercise 2 -----
+#write a func named largest that takes a list of numbers as an argument and returns the largest number in that list
+
+# ----- Exercise 3 -----
+#Write a func named occurances that takes two string arguments as input and counts the number of occurances of the second string inside the first string
+
+# ----- Exercise 4 -----
+#Write a func name product that takes an arbitrary number of numbers, multiplies them all together, and returns the product (HINT: REVIEW NOTES ON *args)

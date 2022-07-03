efe9f22d10a7f0234f5edfc33535cc84236d6fbd
Joan Renfroe <joan.renfroe@gmail.com>
2022-07-02 21:04:04 -0700

    everything is working

--- py_func_lab.py
+++ py_func_lab.py
@@ -47,7 +47,7 @@ occurrences('fleep floop', 'fe')
 
 
 
-# 4. Write a function named product that takes an arbitrary number of numbrs, multiplies them all together, and returns the product (review your notes on *args)
+# 4. Write a function named product that takes an arbitrary number of numbers, multiplies them all together, and returns the product (review your notes on *args)
 # product(-1, 4)      #returns -4
 # product(2, 5, 5)    #returns 50
 # product(4, 0.5, 5)  # returns 10.0
